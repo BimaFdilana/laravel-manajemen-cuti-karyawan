4177408c4e8639c72e995e6e6fb37230ba140114
BimaFdilana <bfadilana@gmail.com>
2025-07-07 15:23:57 +0700

    new-1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,15 +5,13 @@
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
     <title>@yield('title') &mdash; Stisla</title>
-    <link rel="icon" type="image/x-icon" href="{{ asset('img/logo.png') }}">
+    <link rel="icon" type="image/x-icon" href="{{ asset('img/logo3.png') }}">
 
     <link rel="stylesheet" href="{{ asset('library/bootstrap/dist/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
         integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
     @stack('style')
-
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <link rel="stylesheet" href="{{ asset('css/components.css') }}">
 
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-94034622-3"></script>
--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
     <title>@yield('title') &mdash; Stisla</title>
-    <link rel="icon" type="image/x-icon" href="{{ asset('img/logo.png') }}">
+    <link rel="icon" type="image/x-icon" href="{{ asset('img/logo3.png') }}">
 
     <!-- General CSS Files -->
     <link rel="stylesheet" href="{{ asset('library/bootstrap/dist/css/bootstrap.min.css') }}">
